8a314aa05dac540a3955a70dde9b4d530c5f4f14
Adrien Deveaux <adrien.dvx@gmail.com>
2022-05-23 14:22:07 +0200

    initial commit

---
+++ JS/data.js
@@ -0,0 +1,20 @@
+const musicSampledIn = [
+  {
+    artiste: "Edwin Birdsong",
+    title: "Cola Bottle Baby",
+    genre: "Soul / Funk / Disco",
+    url: "https://www.youtube.com/watch?v=fiD39jo5Yo4&t=87s&ab_channel=EdwinBirdsong-Topic",
+    sample: "sample appears at 0:00 and 4:51",
+    hasBeenPlayed: false,
+    sampleIn: "Harder, Better, Faster, Stronger",
+  },
+  {
+    artiste: "Golden Sounds",
+    title: "Zamina Mina (Zangalewa)",
+    genre: "world",
+    url: "https://www.youtube.com/watch?v=EpHQAw6eCOY&ab_channel=TheAfricanMusicCharts",
+    sample: "sampole appears at 0:10 and 7:30",
+    hasBeenPlayed: false,
+    sampleIn: "Waka Waka (This Time for Africa)",
+  },
+];
---
+++ JS/databis.js
@@ -0,0 +1,18 @@
+const musicsSampleOf = [
+    {
+      artiste: "Daft Punk",
+      title: "Harder, Better, Faster, Stronger",
+      genre: "Electronic / Dance",
+      url: "https://www.youtube.com/watch?v=gAjR4_CbPpQ&t=2s&ab_channel=DaftPunk",
+      sample: "sample appears at 0:05 and 0:20 (and throughout)",
+      hasBeenPlayed: false
+    },
+    {
+        artiste:"Shakira",
+        title: "Waka Waka (This Time for Africa)",
+        genre: "Rock / Pop",
+        url: "https://www.youtube.com/watch?v=pRpeEdMmmQ0&feature=emb_title&ab_channel=shakiraVEVO",
+        sample: "sample appears at 0:59 (and throughout)",
+        hasBeenPlayed: false
+    }
+  ];
